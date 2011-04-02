01cae29681cca03d7777ceeac58d2a91a5100df6
David Vallner <david@vallner.net>
2011-04-02 15:50:29 +0200

    domain: make EntityBase serializable, and implement equals() and hashCode()

--- src/main/java/reevent/domain/EntityBase.java
+++ src/main/java/reevent/domain/EntityBase.java
@@ -4,10 +4,11 @@ import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
+import java.io.Serializable;
 import java.util.UUID;
 
 @MappedSuperclass
-public abstract class EntityBase {
+public abstract class EntityBase implements Serializable {
     @Id
     @Column(columnDefinition = "UUID")
     protected UUID id = UUID.randomUUID();
@@ -28,5 +29,22 @@ public abstract class EntityBase {
 
     public void setVersion(Integer version) {
         this.version = version;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EntityBase)) return false;
+
+        EntityBase that = (EntityBase) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
