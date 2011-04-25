4c934105102b4ec345063c70f650c61154578360
David Vallner <david@vallner.net>
2011-04-26 00:04:09 +0200

    Implement authentication

--- src/main/java/reevent/web/ReEventSession.java
+++ src/main/java/reevent/web/ReEventSession.java
@@ -1,11 +1,29 @@
 package reevent.web;
 
+import org.apache.commons.lang.Validate;
 import org.apache.wicket.Session;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebSession;
 import org.apache.wicket.authroles.authorization.strategies.role.Roles;
+import org.apache.wicket.injection.Injector;
 import org.apache.wicket.request.Request;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import reevent.domain.User;
+import reevent.domain.UserRole;
+import reevent.service.UserService;
 
 public class ReEventSession extends AuthenticatedWebSession {
+    /**
+     * Profile of the currently signed in user.
+     */
+    User userSignedIn;
+
+    @SpringBean
+    UserService userService;
+
+    {
+        Injector.get().inject(this);
+    }
+
     public static ReEventSession get() {
         return (ReEventSession) Session.get();
     }
@@ -16,6 +34,29 @@ public class ReEventSession extends AuthenticatedWebSession {
 
     @Override
     public Roles getRoles() {
-        throw new UnsupportedOperationException(); // todo implement -dV
+        if (!isSignedIn()) {
+            // guest access
+            return new Roles();
+        } else {
+            Validate.notNull(userSignedIn);
+
+            Roles roles = new Roles();
+            for (UserRole role : userSignedIn.getRoles()) {
+                roles.add(role.name());
+            }
+            return roles;
+        }
+    }
+
+    @Override
+    public boolean authenticate(String username, String password) {
+        userSignedIn = userService.authenticate(username, password);
+        return userSignedIn != null;
+    }
+
+    @Override
+    public void signOut() {
+        userSignedIn = null;
+        super.signOut();    //To change body of overridden methods use File | Settings | File Templates.
     }
 }
