b00050bbed4003a0dc6a49fc859f07d8a3ac10bc
David Vallner <david@vallner.net>
2011-06-13 10:55:28 +0200

    Revert "wip"
    This reverts commit 25f3556140a0e52eda3e2902c0e5414378b0b73e.

--- src/main/java/reevent/web/account/changeAccount.java
+++ src/main/java/reevent/web/account/changeAccount.java
@@ -1,21 +1,17 @@
 package reevent.web.account;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.validator.AbstractValidator;
-import reevent.dao.UserDao;
 import reevent.domain.User;
 import reevent.service.UserService;
 import reevent.web.ReEventApplication;
-import reevent.web.ReEventSession;
 import reevent.web.Template;
 
 import java.util.List;
@@ -26,17 +22,13 @@ import static java.util.Arrays.asList;
 public class changeAccount extends Template {
 	
 	Form<User> changeUserForm;
-    Label username;
+    TextField<String> username;
     TextField<String> password;
     TextField<String> passwordVerify;
     TextField<String> firstName;
     TextField<String> lastName;
-
-    @SpringBean
+    
     UserService users;
-
-    @SpringBean
-    UserDao userDao;
 
 	public changeAccount(){
         CompoundPropertyModel<User> formModel = new CompoundPropertyModel<User>(ReEventSession.get().getModUserSignedIn());
@@ -62,9 +54,9 @@ public class changeAccount extends Template {
             }
         });
 
-        changeUserForm.add(password = new PasswordTextField("password", new Model()));
+        changeUserForm.add(password = new PasswordTextField("password", Model.<String>of()));
 
-        changeUserForm.add(passwordVerify = new PasswordTextField("passwordVerify", new Model()));
+        changeUserForm.add(passwordVerify = new PasswordTextField("passwordVerify", Model.<String>of()));
         passwordVerify.add(new AbstractValidator<String>() {
             @Override
             protected void onValidate(IValidatable<String> field) {
@@ -76,9 +68,9 @@ public class changeAccount extends Template {
             }
         });
 
-        changeUserForm.add(firstName = new TextField<String>("firstName"));
+        changeUserForm.add(firstName = new TextField<String>("firstName", formModel.<String>bind("firstName")));
         
-        changeUserForm.add(lastName = new TextField<String>("lastName"));
+        changeUserForm.add(lastName = new TextField<String>("lastName", formModel.<String>bind("lastName")));
 
         // required fields
         List<TextField<String>> required = asList(firstName, lastName);
