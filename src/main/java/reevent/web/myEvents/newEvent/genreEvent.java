4829378937134bd2c5ff70ae2272100f86eda32c
Patrick Neubauer <patymedia@gmail.com>
2011-06-25 16:05:38 +0200

    * Event Genre Search [WORKS]

--- src/main/java/reevent/web/myEvents/newEvent/genreEvent.java
+++ src/main/java/reevent/web/myEvents/newEvent/genreEvent.java
@@ -2,9 +2,11 @@ package reevent.web.myEvents.newEvent;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.util.convert.IConverter;
@@ -19,11 +21,16 @@ import reevent.web.convert.DateTimeConverter;
 import reevent.web.myEvents.myEvents;
 
 import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class genreEvent extends Template {
 
 	ListView<Event> myEventList;
+	
+	private static final List GENRES = Arrays.asList(new String[] {"Concerts and Tour Dates", "Conferences and Tradeshows", "Education", "Kids and Family", "Festivals", "Film", "Food and Wine", "Fundraising and Charity", "Art Galleries and Exhibits", "Health and Wellness", "Literary and Books", "Museums and Attractions", "Neighborhood", "Business and Networking", "Nightlife and Singles", "University and Alumni", "Organizations and Meetups", "Outdoors and Recreation", "Performing Arts", "Pets", "Politics and Activism", "Sales and Retail", "Science", "Religion and Spirituality", "Sports", "Technology", "Other and Miscellaneous" });
+	DropDownChoice<String> genre;
 	
 	Link detailEvent;
 	
@@ -37,6 +44,16 @@ public class genreEvent extends Template {
 	public genreEvent(String eventGenre){
 		
 	add(new Label("genreName", eventGenre));
+	add(genre = new DropDownChoice<String>("genre", new Model(eventGenre), GENRES) {
+		@Override
+		protected boolean wantOnSelectionChangedNotifications() {
+			return true;
+		};
+		protected void onSelectionChanged(String newSelection) {
+			setResponsePage(new genreEvent(newSelection));
+		};
+		
+	});
    
     add(myEventList = new ListView<Event>("myEventList", eventDao.queryByEventgenre(eventGenre)) {
         @Override
