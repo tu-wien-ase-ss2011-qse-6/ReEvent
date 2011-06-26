fa381e98a3a610d7355968673ad7830c12a04555
David Vallner <david@vallner.net>
2011-06-26 18:24:50 +0200

    show correct location when editing event

--- src/main/webapp/resource/newLocation.js
+++ src/main/webapp/resource/newLocation.js
@@ -1,51 +1,61 @@
-var map = null;
-var geocoder = null;
-var marker;
+$(function() {
+    var map = null;
+    var geocoder = null;
+    var marker;
 
-newLocation = {
-    initialize: function initialize() {
-        if (GBrowserIsCompatible()) {
-            console.info("initialising myGoogleMaps");
-            map = new GMap2(document.getElementById("map_canvas"));
-            console.debug(map);
-            map.setCenter(new GLatLng(37.4419, -122.1419), 1);
-            map.setUIToDefault();
-            geocoder = new GClientGeocoder();
-        }
-    },
+    var $lat = $('.newEventForm .latitude')
+    var $lng = $('.newEventForm .longitude')
 
-    showAddress: function showAddress() {
-        var address = $('.locationAddress').val()
-        if (geocoder) {
-            geocoder.getLatLng(
-                    address,
-                    function(point) {
-                        if (!point) {
-                            alert(address + " not found");
-                        } else {
-                            map.setCenter(point, 15);
-                            marker = new GMarker(point, {draggable: true});
-                            map.addOverlay(marker);
-                            GEvent.addListener(marker, "dragend", function() {
-                                marker.openInfoWindowHtml(marker.getLatLng().toUrlValue(6));
-                                $('.newEventForm .latitude').val(marker.getLatLng().lat())
-                                $('.newEventForm .longitude').val(marker.getLatLng().lng())
-                            });
-                            GEvent.addListener(marker, "click", function() {
-                                marker.openInfoWindowHtml(marker.getLatLng().toUrlValue(6));
-                                $('.newEventForm .latitude').val(marker.getLatLng().lat())
-                                $('.newEventForm .longitude').val(marker.getLatLng().lng())
-                            });
-                            GEvent.trigger(marker, "click");
-                            $('.newEventForm .latitude').val(point.lat())
-                            $('.newEventForm .longitude').val(point.lng())
-                            
-                        }
-                    }
-                    );
+    function readMarker() {
+        marker.openInfoWindowHtml(marker.getLatLng().toUrlValue(6));
+        $lat.val(marker.getLatLng().lat())
+        $lng.val(marker.getLatLng().lng())
+    }
+
+    function replaceMarker(point) {
+        if (!point) {
+            alert(address + " not found");
+        } else {
+            if (marker != null) {
+                map.removeOverlay(marker);
+            }
+            map.setCenter(point, 15);
+            marker = new GMarker(point, {draggable: true});
+            map.addOverlay(marker);
+            GEvent.addListener(marker, "dragend", readMarker);
+            GEvent.addListener(marker, "click", readMarker);
+            readMarker();
         }
     }
 
-}
+    function showAddress() {
+        var address = $('.locationAddress').val()
+        if (geocoder) {
+            geocoder.getLatLng(address, replaceMarker);
+        }
+    }
 
-$(newLocation.initialize);
+
+    if (GBrowserIsCompatible()) {
+        console.info("initialising myGoogleMaps");
+        map = new GMap2(document.getElementById("map_canvas"));
+        console.debug(map);
+
+        var lat = $lat.val()
+        var lng = $lng.val()
+
+        if (lat && lng) {
+            lat = Number(lat)
+            lng = Number(lng)
+            var point = new GLatLng(lat, lng)
+            map.setCenter(point, 1)
+            replaceMarker(point)
+        } else {
+            map.setCenter(new GLatLng(37.4419, -122.1419), 1);
+        }
+
+        map.setUIToDefault();
+        geocoder = new GClientGeocoder();
+    }
+
+})
