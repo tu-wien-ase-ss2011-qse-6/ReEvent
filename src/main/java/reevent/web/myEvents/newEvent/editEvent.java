c22e65ff1d98db250af4f0f5b92761920d17f758
David Vallner <david@vallner.net>
2011-06-26 17:15:05 +0200

    Allow editing an object with newEvent

--- src/main/java/reevent/web/myEvents/newEvent/editEvent.java
+++ src/main/java/reevent/web/myEvents/newEvent/editEvent.java
@@ -21,60 +21,53 @@ import java.util.Date;
 @AuthorizeInstantiation("USER")
 public class editEvent extends myEvents {
 
-	ListView<Event> myEventList;
-	
-	Link detailEvent;
-	Link deleteEvent;
-	
-	@SpringBean
-	EventService events;
-	 
-	public editEvent(){
-		
-    User user = ReEventSession.get().getModUserSignedIn().getObject();
-    
-    add(myEventList = new ListView<Event>("myEventList", events.getByUser(user)) {
-        @Override
-        protected void populateItem(final ListItem<Event> item) {
-        	
-            item.add(new Label("eventName", new PropertyModel(item.getModel(), "name")));
-            
-            item.add(detailEvent = new Link("detailEvent"){
-            	public void onClick() {
-                    
-                    Event obj = (Event) item.getModelObject();
-                    setResponsePage(new detailEvent(obj));
-            		
-            	}
-            });
-            
-            item.add(deleteEvent = new Link("deleteEvent"){
-            	public void onClick() {
-            		
-            		Event obj = (Event) item.getModelObject();
-                    setResponsePage(new deleteEvent(obj));
-            		
-            	}
-            });
-            
-            item.add(new Label("eventStart", new PropertyModel(item.getModel(), "start")) {
-                @Override
-                public <C> IConverter<C> getConverter(Class<C> type) {
-                    if (!Date.class.isAssignableFrom(type)) {
-                        throw new IllegalArgumentException("Can't convert from types other than Date");
+    ListView<Event> myEventList;
+
+    Link<Event> detailEvent;
+    Link<Event> deleteEvent;
+
+    @SpringBean
+    EventService events;
+
+    public editEvent() {
+
+        User user = ReEventSession.get().getModUserSignedIn().getObject();
+
+        add(myEventList = new ListView<Event>("myEventList", events.getByUser(user)) {
+            @Override
+            protected void populateItem(final ListItem<Event> item) {
+
+                item.add(new Label("eventName", new PropertyModel(item.getModel(), "name")));
+
+                item.add(detailEvent = new Link<Event>("detailEvent", item.getModel()) {
+                    public void onClick() {
+                        setResponsePage(new detailEvent(getModelObject()));
                     }
-                    return (IConverter<C>) DateTimeConverter.both(DateFormat.SHORT, DateFormat.SHORT);
-                }
-            });
-            
-            
-            item.add(new Label("eventPerformer", new PropertyModel(item.getModel(), "performer")));
-            item.add(new Label("eventLocationName", new PropertyModel(item.getModel(), "locationName")));
-            
-        }
-    });
-		
-	}
-	
-	
+                });
+
+                item.add(deleteEvent = new Link<Event>("deleteEvent", item.getModel()) {
+                    public void onClick() {
+                        setResponsePage(new deleteEvent(getModelObject()));
+                    }
+                });
+
+                item.add(new Label("eventStart", new PropertyModel(item.getModel(), "start")) {
+                    @Override
+                    public <C> IConverter<C> getConverter(Class<C> type) {
+                        if (!Date.class.isAssignableFrom(type)) {
+                            throw new IllegalArgumentException("Can't convert from types other than Date");
+                        }
+                        return (IConverter<C>) DateTimeConverter.both(DateFormat.SHORT, DateFormat.SHORT);
+                    }
+                });
+
+
+                item.add(new Label("eventPerformer", new PropertyModel(item.getModel(), "performer")));
+                item.add(new Label("eventLocationName", new PropertyModel(item.getModel(), "locationName")));
+            }
+        });
+
+    }
+
+
 }
--- src/main/java/reevent/web/myEvents/newEvent/newEvent.java
+++ src/main/java/reevent/web/myEvents/newEvent/newEvent.java
@@ -3,20 +3,18 @@ package reevent.web.myEvents.newEvent;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.FormComponent;
-import org.apache.wicket.markup.html.form.HiddenField;
-import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.markup.html.form.upload.FileUpload;
 import org.apache.wicket.markup.html.form.upload.FileUploadField;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.validator.AbstractValidator;
 import org.apache.wicket.validation.validator.UrlValidator;
+import reevent.dao.EventDao;
 import reevent.domain.Event;
 import reevent.domain.User;
 import reevent.domain.media.MediaBase;
@@ -34,7 +32,7 @@ import static java.util.Arrays.asList;
 import static reevent.util.WicketErrorUtil.getError;
 
 @AuthorizeInstantiation("USER")
-public class newEvent extends myEvents{
+public class newEvent extends myEvents {
 	
 	private static final List GENRES = Arrays.asList(new String[] {"Concerts and Tour Dates", "Conferences and Tradeshows", "Education", "Kids and Family", "Festivals", "Film", "Food and Wine", "Fundraising and Charity", "Art Galleries and Exhibits", "Health and Wellness", "Literary and Books", "Museums and Attractions", "Neighborhood", "Business and Networking", "Nightlife and Singles", "University and Alumni", "Organizations and Meetups", "Outdoors and Recreation", "Performing Arts", "Pets", "Politics and Activism", "Sales and Retail", "Science", "Religion and Spirituality", "Sports", "Technology", "Other and Miscellaneous" });
 
@@ -57,7 +55,11 @@ public class newEvent extends myEvents{
     List<Event> myEvents;
 
     @SpringBean
-    EventService events;
+    EventService eventService;
+
+    @SpringBean
+    EventDao eventDao;
+
     Event event;
 
     @SpringBean
@@ -65,84 +67,96 @@ public class newEvent extends myEvents{
 
     Link newLocationLink;
 
-    public newEvent(){
+    boolean update = false;
 
-        CompoundPropertyModel<Event> formModel = new CompoundPropertyModel<Event>(new Event());
+    public newEvent(){
+        this(Model.<Event>of(new Event()));
+        update = true;
+    }
+    public newEvent(IModel<Event> model) {
+        setDefaultModel(model);
+        CompoundPropertyModel<Event> formModel = new CompoundPropertyModel<Event>(model);
 
         add(newEventForm = new Form<Event>("newEventForm", formModel) {
-            @Override
-            protected void onSubmit() {
+                    @Override
+                    protected void onSubmit() {
 
-                event = newEventForm.getModelObject();
-                User user = ReEventSession.get().getModUserSignedIn().getObject();
+                        Event event = newEventForm.getModelObject();
+                        User user = ReEventSession.get().getModUserSignedIn().getObject();
 
-                MediaBase mainPicture = null;
-                try {
-                    String url = pictureUrl.getModelObject();
-                    FileUpload file = pictureFile.getFileUpload();
-                    if (!StringUtils.isBlank(url)) {
-                        mainPicture = mediaService.create(url, user);
-                    } else if (file != null) {
-                        mainPicture = mediaService.create(file, user);
+                        MediaBase mainPicture = null;
+                        try {
+                            String url = pictureUrl.getModelObject();
+                            FileUpload file = pictureFile.getFileUpload();
+                            if (!StringUtils.isBlank(url)) {
+                                mainPicture = mediaService.create(url, user);
+                            } else if (file != null) {
+                                mainPicture = mediaService.create(file, user);
+                            }
+                        } catch (MediaUploadException e) {
+                            error(getError(this, "error", e));
+                        }
+                        event.setMainPicture(mainPicture);
+
+                        if (update) {
+                            eventDao.update(event);
+                        } else {
+                            event.setCreatedBy(user);
+                            eventService.create(event);
+                        }
+
+                        ReEventSession.get().info(getString("success"));
+
+                        HomePage page = new HomePage();
+                        setResponsePage(page);
+
                     }
-                } catch (MediaUploadException e) {
-                    error(getError(this, "error", e));
+                });
+
+                newEventForm.add(name = new TextField<String>("name"));
+
+                newEventForm.add(start = new TextField<String>("start"));
+
+                name.add(new AbstractValidator<String>(){
+
+                    @Override
+                    protected void onValidate(IValidatable<String> field) {
+                        if (!eventService.isAvailable(field.getValue())) {
+                            this.error(field, "event.not.available");
+                        }
+                    }
+
+                });
+
+                newEventForm.add(performer = new TextField<String>("performer"));
+
+                newEventForm.add(genre = new DropDownChoice<String>("genre", GENRES));
+
+                newEventForm.add(locationName = new TextField<String>("locationName"));
+
+                newEventForm.add(locationAddress = new TextField<String>("locationAddress"));
+
+                // add the location hidden fields to the form
+                newEventForm.add(latitude = new HiddenField<String>("latitude"));
+                newEventForm.add(longitude = new HiddenField<String>("longitude"));
+
+                newEventForm.add(pictureUrl = new TextField<String>("pictureUrl", new Model()));
+                pictureUrl.add(new UrlValidator());
+
+                newEventForm.add(pictureFile = new FileUploadField("pictureFile", new Model()));
+
+
+                // required fields
+                List<? extends FormComponent<?>> fields = asList(
+                        name, performer, start, genre,
+                        locationName, locationAddress);
+                addFormLabels(pictureUrl,  pictureFile); // add nonrequired labels
+
+                addFormLabels(fields);
+
+                for (FormComponent fc : fields) {
+                    fc.setRequired(true);
                 }
-                event.setMainPicture(mainPicture);
-                event.setCreatedBy(user);
-                events.create(event);
-
-                HomePage page = new HomePage();
-                page.info(getString("success"));
-                setResponsePage(page);
-
-            }
-        });
-
-        newEventForm.add(name = new TextField<String>("name"));
-
-        newEventForm.add(start = new TextField<String>("start"));
-
-        name.add(new AbstractValidator<String>(){
-
-            @Override
-            protected void onValidate(IValidatable<String> field) {
-                if (!events.isAvailable(field.getValue())) {
-                    this.error(field, "event.not.available");
-                }
-            }
-
-        });
-
-        newEventForm.add(performer = new TextField<String>("performer"));
-
-        newEventForm.add(genre = new DropDownChoice<String>("genre", GENRES));
-
-        newEventForm.add(locationName = new TextField<String>("locationName"));
-
-        newEventForm.add(locationAddress = new TextField<String>("locationAddress"));
-
-        // add the location hidden fields to the form
-        newEventForm.add(latitude = new HiddenField<String>("latitude"));
-        newEventForm.add(longitude = new HiddenField<String>("longitude"));
-        
-        newEventForm.add(pictureUrl = new TextField<String>("pictureUrl", new Model()));
-        pictureUrl.add(new UrlValidator());
-        
-        newEventForm.add(pictureFile = new FileUploadField("pictureFile", new Model()));
-
-
-        // required fields
-        List<? extends FormComponent<?>> fields = asList(
-                name, performer, start, genre,
-                locationName, locationAddress);
-        addFormLabels(pictureUrl,  pictureFile); // add nonrequired labels
-        
-        addFormLabels(fields);
-
-        for (FormComponent fc : fields) {
-            fc.setRequired(true);
-        }
 
     }
 }
