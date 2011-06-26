e78607a9589a8a73ed57f19403aa531375504088
Patrick Neubauer <patymedia@gmail.com>
2011-06-26 20:47:47 +0200

    * Finished Admin Panel (delete, actually disable, User works)

--- src/main/java/reevent/web/adminPanel/adminPanel.java
+++ src/main/java/reevent/web/adminPanel/adminPanel.java
@@ -1,20 +1,63 @@
 package reevent.web.adminPanel;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import reevent.dao.UserDao;
+import reevent.domain.User;
+import reevent.service.UserService;
+import reevent.web.ReEventSession;
 import reevent.web.Template;
+
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.ComponentPropertyModel;
 
 @AuthorizeInstantiation("ADMIN")
 public class adminPanel extends Template {
 
-	Link editUserLink;
-	Link deleteUserLink;
+	ListView<User> myUserList;
 	
-	public adminPanel() {
+	@SpringBean
+	UserDao userDao;
+	
+	@SpringBean
+	UserService userService;
+	
+	public adminPanel(){
+	
+		User adminuser = ReEventSession.get().getModUserSignedIn().getObject();
 		
-		add(editUserLink = new BookmarkablePageLink("editUserLink", editUser.class));
-		add(deleteUserLink = new BookmarkablePageLink("deleteUserLink", deleteUser.class));
+		add(myUserList = new ListView<User>("myUserList", userDao.findAll()) {
+			
+			@Override
+			protected void populateItem(final ListItem<User> item) {
+				
+				
+					item.add(new Label("userName", new ComponentPropertyModel("username")));
+					
+					item.add(new Link("deleteUser"){
+		            	public void onClick() {
+		            		
+		            		User obj = (User) item.getModelObject();
+		            		userService.disable(obj.getId());
+		            		setResponsePage(new adminPanel());
+		            		
+		            	}
+		            });
+					
+					// do not display disabled users
+					item.setVisible(item.getModelObject().isEnabled());
+				
+				
+				
+				
+			}
+			
+			
+		});
 		
 	}
 }
