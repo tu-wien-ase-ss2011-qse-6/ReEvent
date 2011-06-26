53045101fd59f96577674f368931e5fffa6df612
Patrick Neubauer <patymedia@gmail.com>
2011-06-26 14:53:22 +0200

    * Delete Feedback [WIP]

--- src/main/java/reevent/web/myEvents/FeedbackDisplay.java
+++ src/main/java/reevent/web/myEvents/FeedbackDisplay.java
@@ -1,11 +1,17 @@
 package reevent.web.myEvents;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.ComponentPropertyModel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
 import reevent.domain.Feedback;
+import reevent.web.ReEventApplication;
+import reevent.web.ReEventSession;
 import reevent.web.StyledPanel;
+import reevent.web.myEvents.newEvent.detailEvent;
 
 /**
  * A block that shows the main picture and basic data about an event.
@@ -15,6 +21,8 @@ public class FeedbackDisplay extends StyledPanel {
     Label text;
     Label rating;
     Label createdBy;
+    
+    Link deleteFeedback;
 
     public FeedbackDisplay(String id, final IModel<Feedback> feedback) {
         super(id, new CompoundPropertyModel<Feedback>(feedback));
@@ -22,6 +30,18 @@ public class FeedbackDisplay extends StyledPanel {
         this.add(text = new Label("text"));
         this.add(rating = new Label("rating"));
         this.add(createdBy = new Label("createdBy", new ComponentPropertyModel("createdBy.username")));
+        
+        this.add(deleteFeedback = new Link("deleteFeedback", new Model()){
+        	public void onClick() {
+        		
+                setResponsePage(this.getPage());
+        		
+        	}
+        });
+        
+        if (ReEventSession.get().getUserSignedIn() != null){
+        	deleteFeedback.setVisible(ReEventSession.get().getUserSignedIn().equals(feedback.getObject().getCreatedBy().getId()));
+        }
         
     }
 }
